94faee8bb1b3d18b8f8605ab439e197128f38539
borbier <yuttakhanbaingen@gmail.com>
2020-09-11 14:43:43 +0700

    Fix error logged from eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout";
 import { observer, inject } from "mobx-react";
 
 const IndexPage = inject("store")(
-  observer((props) => {
+  observer((props: any) => {
     return (
       <Layout title="Home | Next.js + TypeScript Example">
         <div className="shadow-lg px-6 py-4 bg-gray-200">
