23ab4b423a1c5c3b320ace5104c7997522a88b92
borbier <yuttakhanbaingen@gmail.com>
2020-09-11 14:28:22 +0700

    Add observer to page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
+import { Provider } from "mobx-react";
+import { useStore } from "../stores";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const store = useStore(pageProps.initialState);
+
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <div className="shadow-lg px-6 py-4 bg-gray-200">
-      <div className="text-2xl">Next.js is up and running 👋</div>
-      <hr />
-      <p>
-        Go to &nbsp;
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </div>
-  </Layout>
+import { observer, inject } from "mobx-react";
+
+const IndexPage = inject("store")(
+  observer((props) => {
+    return (
+      <Layout title="Home | Next.js + TypeScript Example">
+        <div className="shadow-lg px-6 py-4 bg-gray-200">
+          <div className="text-2xl">{props.store.data.title}</div>
+          <hr />
+          <p>
+            Go to &nbsp;
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </p>
+        </div>
+      </Layout>
+    );
+  })
 );
 
 export default IndexPage;
